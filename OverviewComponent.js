aaca26c77a5aef1b11a46a2b9e1b1100df193f01
Rytryx <imrytry@gmail.com>
2024-01-12 16:56:00 +0100

    Saving Progress

--- OverviewComponent.js
+++ OverviewComponent.js
@@ -1,16 +1,29 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Appbar, Divider } from 'react-native-paper';
+
+const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
 
 const OverviewComponent = () => {
   const insets = useSafeAreaInsets();
 
   return (
-    <SafeAreaProvider>
-      <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom }]}>
-        <Text style={styles.text}>OverviewComponent</Text>
-      </View>
-    </SafeAreaProvider>
+    <View>
+      <Appbar.Header>
+       <Appbar.Content title="My Reflexions" />
+        <Appbar.Action icon="magnify" onPress={() => {}} />
+        <Appbar.Action icon="calendar" onPress={() => {}} />
+      </Appbar.Header>
+      <Text>Lemon</Text>
+      <Divider />
+      <Text>Mango</Text>
+      <Divider />
+      <Text>Mango</Text>
+      <Divider />
+      <Text>Mango</Text>
+      <Divider />
+  </View>
   );
 };
 
